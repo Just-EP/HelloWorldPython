9e3c22973424d71472504211ea2b2afaab13ffb9
Just-EP <lovelive7185@outlook.com>
2020-02-09 22:53:28 +0800

    按规范修改

--- demo/Useffmpeg.py
+++ demo/Useffmpeg.py
@@ -1,19 +1,26 @@
+#!/usr/bin/python3
 # 使用ffmpeg将脚本所在目录下的wav音频转换为mp3
-if __name__ == '__main__':
+
+
+def main():
     import os
     # 获取脚本所在路径
     # ffmpeg -i input.wav -f mp3 -acodec libmp3lame -y output.mp3
     path = os.getcwd()
-    suffixWav = '.wav'
-    suffixMP3 = '.mp3'
+    suffix_wav = '.wav'
+    suffix_mp3 = '.mp3'
     # os.walk(path) 路径 路径下所有子文件夹 路径下所有所有文件
     ftd = []
     for p, d, files in os.walk(path):
         for file in files:
-            if file[file.index('.'):] == suffixWav:
+            if file[file.index('.'):] == suffix_wav:
                 # ftd.append(path+os.sep+file)
                 ftd.append(file)
     for name in ftd:
-        mp3name = name.replace(suffixWav, suffixMP3)
-        cmd = 'ffmpeg -i '+name+' -f mp3 -acodec libmp3lame -y '+mp3name
+        mp3_name = name.replace(suffix_wav, suffix_mp3)
+        cmd = 'ffmpeg -i ' + name + ' -f mp3 -acodec libmp3lame -y ' + mp3_name
         os.system(cmd)
+
+
+if __name__ == '__main__':
+    main()
