a172a6c74613e12b50af83ecf46f9df5c49cdfa7
Just-EP <lovelive7185@outlook.com>
2020-05-03 09:07:45 +0800

    转为aac格式

--- demo/Useffmpeg.py
+++ demo/Useffmpeg.py
@@ -7,18 +7,20 @@ def main():
     # 获取脚本所在路径
     # ffmpeg -i input.wav -f mp3 -acodec libmp3lame -y output.mp3
     path = os.getcwd()
-    suffix_wav = '.wav'
-    suffix_mp3 = '.mp3'
+    suffix_input = '.wav'
+    suffix_output = '.aac'
     # os.walk(path) 路径 路径下所有子文件夹 路径下所有所有文件
     ftd = []
     for p, d, files in os.walk(path):
         for file in files:
-            if file[file.index('.'):] == suffix_wav:
+            if file[file.index('.'):] == suffix_input:
                 # ftd.append(path+os.sep+file)
                 ftd.append(file)
     for name in ftd:
-        mp3_name = name.replace(suffix_wav, suffix_mp3)
-        cmd = 'ffmpeg -i ' + name + ' -f mp3 -acodec libmp3lame -y ' + mp3_name
+        output_name = name.replace(suffix_input, suffix_output)
+        cmd = 'ffmpeg -i ' + name + ' -c:a aac ' + output_name
+        # cmd = 'ffmpeg -i ' + name + ' -f mp3 -acodec libmp3lame -y ' + output_name
+        # ffmpeg -i a.wav  -c:a aac a.aac
         os.system(cmd)
 
 
