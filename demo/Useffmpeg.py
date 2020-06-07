69331ed8a9fb71da0b855042dfecf84ff07e0ad4
Just-EP <lovelive7185@outlook.com>
2020-06-07 23:14:49 +0800

    选择flac格式,并修改后缀无法截取的bug

--- demo/Useffmpeg.py
+++ demo/Useffmpeg.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 使用ffmpeg将脚本所在目录下的wav音频转换为mp3
+# 使用ffmpeg将脚本所在目录下的wav音频转换为其他占用磁盘空间较小的格式
 
 
 def main():
@@ -8,19 +8,21 @@ def main():
     # ffmpeg -i input.wav -f mp3 -acodec libmp3lame -y output.mp3
     path = os.getcwd()
     suffix_input = '.wav'
-    suffix_output = '.aac'
+    suffix_output = '.flac'
     # os.walk(path) 路径 路径下所有子文件夹 路径下所有所有文件
     ftd = []
     for p, d, files in os.walk(path):
         for file in files:
-            if file[file.index('.'):] == suffix_input:
+            if file[file.rfind('.'):] == suffix_input:
                 # ftd.append(path+os.sep+file)
                 ftd.append(file)
     for name in ftd:
         output_name = name.replace(suffix_input, suffix_output)
-        cmd = 'ffmpeg -i ' + name + ' -c:a aac ' + output_name
+        cmd = 'ffmpeg -i ' + name + ' ' + output_name
+        # cmd = 'ffmpeg -i ' + name + ' -c:a aac ' + output_name
         # cmd = 'ffmpeg -i ' + name + ' -f mp3 -acodec libmp3lame -y ' + output_name
         # ffmpeg -i a.wav  -c:a aac a.aac
+        # ffmpeg -i a.wav a.flac
         os.system(cmd)
 
 
